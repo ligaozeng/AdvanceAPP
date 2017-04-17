041d49af1c2725d533e6ed7216e61a482d4f61c0
ligaozeng <lgz_920@163.com>
2017-04-17 14:27:00 +0800

    modify catalog

--- app/src/main/java/com/meihua/advanceapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/meihua/advanceapp/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.meihua.advanceapp;
+package com.meihua.advanceapp.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.meihua.advanceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/meihua/advanceapp/utils/AppUtil.java
@@ -0,0 +1,10 @@
+package com.meihua.advanceapp.utils;
+
+/**
+ * Created by : lgz
+ * Time : 2017/4/14.
+ * Desc :
+ */
+
+public class AppUtil {
+}
